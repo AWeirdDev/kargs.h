e6493b1b6e07635c00f3fdf532fb2763fd098d47
AWeirdDev <aweirdscratcher@gmail.com>
2026-06-27 10:18:58 +0800

    fucking indent

--- kargs.h
+++ kargs.h
@@ -1,8 +1,5 @@
 #include <stdbool.h>
-
-#ifndef KARGS_ALLOC
 #include <stdlib.h>
-#endif // KARGS_ALLOC
 
 #ifndef KARGS_DEF
 #define KARGS_DEF
@@ -10,9 +7,9 @@
 
 #ifndef KA_TYPE_MAP
 #define KA_TYPE_MAP(X)                                                         \
-  X(int, int)                                                                  \
-  X(string, char *)                                                            \
-  X(boolean, bool)
+    X(int, int)                                                                \
+    X(string, char *)                                                          \
+    X(boolean, bool)
 #endif // KA_TYPE_MAP
 
 #ifndef KA_ERROR_MAP
@@ -22,7 +19,7 @@
 typedef enum {
 
 #define EXPAND(name, type) Ka_ArgType_##name,
-  KA_TYPE_MAP(EXPAND)
+    KA_TYPE_MAP(EXPAND)
 #undef EXPAND
 
 } Ka_ArgType;
@@ -43,162 +40,164 @@ typedef enum {
 #include <stdio.h>
 
 #define ka_panic(reason)                                                       \
-  do {                                                                         \
-    fprintf(stderr, "kargs panicked: %s\n", reason);                           \
-    exit(1);                                                                   \
-  } while (0)
+    do {                                                                       \
+        fprintf(stderr, "kargs panicked: %s\n", reason);                       \
+        exit(1);                                                               \
+    } while (0)
 #endif // ka_panic
 
 // Specifies the behavior when asserting whether the allocation
 // had failed. This just detects null pointers. Stoopid ahh
 #define ka_assert_allocated(ptr)                                               \
-  if ((ptr) == NULL) {                                                         \
-    ka_panic("allocation failed");                                             \
-  }
+    if ((ptr) == NULL) {                                                       \
+        ka_panic("allocation failed");                                         \
+    }
 
 // Required fields for dynamic arrays
 #define KA__DA_FIELDS(type)                                                    \
-  size_t length;                                                               \
-  size_t capacity;                                                             \
-  type *items;
+    size_t length;                                                             \
+    size_t capacity;                                                           \
+    type *items;
 
 #define ka__da_reserve(da, expected_capacity)                                  \
-  do {                                                                         \
-    if ((expected_capacity) > (da)->capacity) {                                \
-      if ((da)->capacity == 0) {                                               \
-        (da)->capacity = 128;                                                  \
-      }                                                                        \
+    do {                                                                       \
+        if ((expected_capacity) > (da)->capacity) {                            \
+            if ((da)->capacity == 0) {                                         \
+                (da)->capacity = 128;                                          \
+            }                                                                  \
                                                                                \
-      while ((expected_capacity) > (da)->capacity) {                           \
-        (da)->capacity *= 2;                                                   \
-      }                                                                        \
+            while ((expected_capacity) > (da)->capacity) {                     \
+                (da)->capacity *= 2;                                           \
+            }                                                                  \
                                                                                \
-      void *ptr = realloc((da)->items, (da)->capacity * sizeof(*(da)->items)); \
-      ka_assert_allocated(ptr);                                                \
+            void *ptr =                                                        \
+                realloc((da)->items, (da)->capacity * sizeof(*(da)->items));   \
+            ka_assert_allocated(ptr);                                          \
                                                                                \
-      (da)->items = ptr;                                                       \
-    }                                                                          \
-  } while (0)
+            (da)->items = ptr;                                                 \
+        }                                                                      \
+    } while (0)
 
 #define ka__da_free(da) free((da)->items)
 
 #define ka__da_append(da, item)                                                \
-  do {                                                                         \
-    ka__da_reserve((da), (da)->length + 1);                                    \
-    (da)->items[(da)->length++] = (item);                                      \
-  } while (0)
+    do {                                                                       \
+        ka__da_reserve((da), (da)->length + 1);                                \
+        (da)->items[(da)->length++] = (item);                                  \
+    } while (0)
 
 typedef struct {
-  KA__DA_FIELDS(char)
+    KA__DA_FIELDS(char)
 } Ka__StringView;
 
 static inline size_t ka__sv_len(Ka__StringView *view) { return view->length; }
 
 static inline void ka__sv_push(Ka__StringView *view, char c) {
-  ka__da_append(view, c);
+    ka__da_append(view, c);
 }
 
 typedef struct {
-  char type; // 0 = SHORT, 1 = LONG
-  Ka__StringView text;
+    char type; // 0 = SHORT, 1 = LONG
+    Ka__StringView text;
 } Ka__FlagName;
 
 static Ka__FlagName ka__parse_flag_name(const char *s) {
-  Ka__StringView collector = {0};
-  int n_dashes = 0;
+    Ka__StringView collector = {0};
+    int n_dashes = 0;
 
-  while (*s) {
-    if (ka__sv_len(&collector) == 0) {
-      if (*s == '-')
-        n_dashes += 1;
-      else if (*s != ' ')
-        ka__sv_push(&collector, *s);
+    while (*s) {
+        if (ka__sv_len(&collector) == 0) {
+            if (*s == '-')
+                n_dashes += 1;
+            else if (*s != ' ')
+                ka__sv_push(&collector, *s);
 
-      if (n_dashes >= 3)
-        ka_panic("expected maximum of two dashes, got 3 or more");
-    } else {
-      if (*s == ' ')
-        break;
+            if (n_dashes >= 3)
+                ka_panic("expected maximum of two dashes, got "
+                         "3 or more");
+        } else {
+            if (*s == ' ')
+                break;
 
-      ka__sv_push(&collector, *s);
+            ka__sv_push(&collector, *s);
+        }
+
+        s++;
     }
 
-    s++;
-  }
-
-  return (Ka__FlagName){
-      .type = n_dashes - 1,
-      .text = collector,
-  };
+    return (Ka__FlagName){
+        .type = n_dashes - 1,
+        .text = collector,
+    };
 }
 
 typedef struct {
-  KA__DA_FIELDS(Ka__FlagName)
+    KA__DA_FIELDS(Ka__FlagName)
 } Ka__FlagNames;
 
 static Ka__FlagNames ka__parse_flag_names(const char *s) {
-  Ka__FlagNames parsed = {0};
+    Ka__FlagNames parsed = {0};
 
-  while (*s) {
-    Ka__FlagName name = ka__parse_flag_name(s);
-    ka__da_append(&parsed, name);
-    s++;
-  }
+    while (*s) {
+        Ka__FlagName name = ka__parse_flag_name(s);
+        ka__da_append(&parsed, name);
+        s++;
+    }
 
-  return parsed;
+    return parsed;
 }
 
 typedef struct {
-  Ka_ArgType type;
-  Ka__FlagNames names;
+    Ka_ArgType type;
+    Ka__FlagNames names;
 
-  const char *description;
+    const char *description;
 
-  // we'll put the parsed result here when there's actually data
-  void *slot;
+    // we'll put the parsed result here when there's actually data
+    void *slot;
 } Ka_Arg;
 
 typedef struct {
-  KA__DA_FIELDS(Ka_Arg)
+    KA__DA_FIELDS(Ka_Arg)
 } Ka_Args;
 
 KARGS_DEF void ka_args_append(Ka_Args *args, Ka_Arg arg) {
-  ka__da_append(args, arg);
+    ka__da_append(args, arg);
 }
 
 /// Frees the arg parser (`Ka_Args`) and all the dynamic arrays within.
 KARGS_DEF void ka_args_free(Ka_Args args) {
-  for (size_t i = 0; i < args.length; i++) {
-    free(args.items[i].slot);
-  }
+    for (size_t i = 0; i < args.length; i++) {
+        free(args.items[i].slot);
+    }
 
-  ka__da_free(&args);
+    ka__da_free(&args);
 }
 
 typedef struct {
-  bool optional;
-  char *description;
+    bool optional;
+    char *description;
 } Ka_ArgOptions;
 
 #define ka__arg_fn_body(args, ctype, type_tag, flag_names, options)            \
-  Ka__FlagNames flags = ka__parse_flag_names((flag_names));                    \
+    Ka__FlagNames flags = ka__parse_flag_names((flag_names));                  \
                                                                                \
-  void *slot = malloc(sizeof(ctype));                                          \
-  ka_assert_allocated(slot);                                                   \
+    void *slot = malloc(sizeof(ctype));                                        \
+    ka_assert_allocated(slot);                                                 \
                                                                                \
-  ka_args_append(args, (Ka_Arg){                                               \
-                           .type = (type_tag),                                 \
-                           .names = flags,                                     \
-                           .description = (options).description,               \
-                           .slot = slot,                                       \
-                       });                                                     \
-  return slot;
+    ka_args_append(args, (Ka_Arg){                                             \
+                             .type = (type_tag),                               \
+                             .names = flags,                                   \
+                             .description = (options).description,             \
+                             .slot = slot,                                     \
+                         });                                                   \
+    return slot;
 
 #define ka__arg_fn(name, ctype, type_tag)                                      \
-  KARGS_DEF ctype *ka__arg_##name(Ka_Args *args, const char *flag_names,       \
-                                  Ka_ArgOptions options) {                     \
-    ka__arg_fn_body(args, ctype, type_tag, flag_names, options)                \
-  }
+    KARGS_DEF ctype *ka__arg_##name(Ka_Args *args, const char *flag_names,     \
+                                    Ka_ArgOptions options) {                   \
+        ka__arg_fn_body(args, ctype, type_tag, flag_names, options)            \
+    }
 
 #define EXPAND(name, ctype) ka__arg_fn(name, ctype, Ka_ArgType_##name)
 KA_TYPE_MAP(EXPAND)
@@ -218,12 +217,12 @@ KA_TYPE_MAP(EXPAND)
 // ```
 
 #define ka_arg_boolean(args, flag_names, ...)                                  \
-  ka__arg_boolean(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
+    ka__arg_boolean(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
 
 #define ka_arg_string(args, flag_names, ...)                                   \
-  ka__arg_string(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
+    ka__arg_string(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
 
 #define ka_arg_int(args, flag_names, ...)                                      \
-  ka__arg_int(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
+    ka__arg_int(args, flag_names, (Ka_ArgOptions){__VA_ARGS__})
 
 #endif // KARGS_IMPLEMENTATION
